052266f9dace467f10e6a5a2f5c489b8b7c64f88
DeveloperIlhombek <ilxomdeveloper@gmail.com>
2026-02-20 10:28:42 +0500

    link

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { redirect } from 'next/navigation'
+import Link from 'next/link'
+// import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-	redirect('/login')
+	// redirect('/login')
+	return (
+		<div>
+			<Link href={'/login'}>Login sahifasi</Link>
+			<Link href={'/register'}>Register sahifasi</Link>
+		</div>
+	)
 }
